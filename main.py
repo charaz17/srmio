b47fa76bffa24921384b797982eb7298f31c2b03
Sylwia Brant <sylwia.brant@o2.p>
2023-05-22 20:14:20 +0200

    Add new files

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
-from demo import demo
-from model import train_model, valid_model
-#import tensorflow as tf
-import tensorflow.compat.v1 as tf
-import sys
-tf.disable_v2_behavior()
+import cv2
+from tensorflow.keras.models import model_from_json
+import numpy as np
 
-flags =  tf.app.flags
-flags.DEFINE_string('MODE', 'demo', 
-                    'Set program to run in different mode, include train, valid and demo.')
-flags.DEFINE_string('checkpoint_dir', './ckpt', 
-                    'Path to model file.')
-flags.DEFINE_string('train_data', './data/fer2013/fer2013.csv',
-                    'Path to training data.')
-flags.DEFINE_string('valid_data', './valid_sets/',
-                    'Path to training data.')
-flags.DEFINE_boolean('show_box', False, 
-                    'If true, the results will show detection box')
-FLAGS = flags.FLAGS
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+font = cv2.FONT_HERSHEY_SIMPLEX
 
-SAMPLE_IMAGE_PATH="./images/sample2.jpg"
+def detect_emotion(img):
+    emotions_list = ["Angry", "Disgust", "Fear", "Happy", "Neutral", "Sad", "Surprise"]
+    return emotions_list[np.argmax(loaded_model.predict(img))]
+
 
 def main():
-  assert FLAGS.MODE in ('train', 'valid', 'demo')
-  
-  if FLAGS.MODE == 'demo':
-    demo(FLAGS.checkpoint_dir, FLAGS.show_box,SAMPLE_IMAGE_PATH)
-  elif FLAGS.MODE == 'train':
-    train_model(FLAGS.train_data)
-  elif FLAGS.MODE == 'valid':
-    valid_model(FLAGS.checkpoint_dir, FLAGS.valid_data)
+    img = cv2.imread("images/h13.jpg")
+    cv2.imshow('Determined emotions', img)
 
+    global loaded_model
+    with open("model.json", "r") as json_file:
+        loaded_model_json = json_file.read()
+        loaded_model = model_from_json(loaded_model_json)
+
+        # load weights into the new model
+        loaded_model.load_weights("model_weights.h5")
+        loaded_model.make_predict_function()
+
+    gray_fr = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray_fr, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        fc = gray_fr[y:y + h, x:x + w]
+
+        roi = cv2.resize(fc, (48, 48))
+        determined_emotion = detect_emotion(roi[np.newaxis, :, :, np.newaxis])
+        print(determined_emotion)
+        cv2.putText(img, determined_emotion, (x, y), font, 1, (255, 255, 0), 2)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    cv2.imshow('Determined emotions', img)
+    cv2.waitKey(0)
+
+
+#
+# if __name__ == '__main__':
+#     # SAMPLE_IMAGE_PATH = sys.argv[1]
+#     img = cv2.imread(cv2.samples.findFile("images/a1.jpg"))
+#     main(img)
 if __name__ == '__main__':
-  SAMPLE_IMAGE_PATH=sys.argv[1]
-  main()
+    main()
